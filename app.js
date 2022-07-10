9398bf44418df687a1b0089f3ed3d74e72ba5064
MadhushaPrasad <madushaprasad21@gmail.com>
2022-07-10 20:50:25 +0530

    create a app.js and requre discord.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  intents: [
+    'GUILDS',
+    'GUILD_MEMBERS',
+    'GUILD_MESSAGES',
+    'GUILD_MESSAGE_REACTIONS',
+    'DIRECT_MESSAGES',
+    'DIRECT_MESSAGE_REACTIONS',
+  ],
+});
